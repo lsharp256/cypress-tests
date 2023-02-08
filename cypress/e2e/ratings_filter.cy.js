fffc3eee5565df76da5f0d12f9fbf4fde9b6cbeb
leroy <Lsharp256@gmail.com>
2023-02-09 00:04:16 +0200

    added function to ratings_filter test

--- cypress/e2e/ratings_filter.cy.js
+++ cypress/e2e/ratings_filter.cy.js
@@ -8,12 +8,22 @@ describe("Price filter", () => {
         cy.get("#filter-rating-4-and-up").click();
         // Verify that the URL includes the minimum price filter of 300
         cy.url().should('include', 'filter-price-min=min&filter-price-max=max&filter-rating=4-and-up').then(() => {
-            // Iterate through each product price element
+
+            // Function to extract the product rating from an element
+            const extractProductRating = ($el) => {
+                const productRating = $el.text();
+                const productNum = parseFloat(productRating.replace(/[^\d.]/g, ''));
+                return productNum;
+            }
+            
+            // Iterate through each product rating element
             cy.get('.product-rating').each(($el) => {
             // Wrap each element in a cy.wrap() so that we can use chaining and the should assertion
             cy.wrap($el).should(($el) => {
-            // Verify that each product rating is above 4.0
-            expect(parseFloat($el.text().replace(/[^\d.]/g, ''))).to.be.above(4.0);
+                // Extract the product rating
+                const rating = extractProductRating($el);
+                // Verify that each product rating is above 4.0
+                expect(rating).to.be.above(4.0);
                 });
             });
         });
